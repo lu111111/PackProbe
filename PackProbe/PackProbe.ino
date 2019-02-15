0a22e77b7c434c781972b8ed0310fe07ffc21624
Lucien Thiriet <lucien.thiriet@AZURDRONES.local>
2019-02-15 13:11:06 +0100

    Compatibility with Arduino Pro Micro / Leonardo

--- PackProbe/PackProbe.ino
+++ PackProbe/PackProbe.ino
@@ -82,7 +82,7 @@ or make it automatic. Suggestions welcome.
 *************************************************************************************/
 
 // Yun Bridge Console Access
-#include <Console.h>
+//#include <Console.h>
 
 // Standard and common non-standard Smart Battery commands
 #define BATTERY_MODE             0x03
@@ -124,28 +124,28 @@ void setup()
   Comment out the next two lines following lines for use with Serial output
   
   **************************************************/
-  Bridge.begin();
-  Console.begin();
+  //Bridge.begin();
+  //Console.begin();
 
   /**************************************************
 
   Uncomment the following lines for use with Serial output
   
   **************************************************/
-  //Serial.begin(115200);  // start serial for output
-  //Serial.println(i2c_init());
-  //pinMode(22,INPUT_PULLUP);
-  //pinMode(23,INPUT_PULLUP);
+  Serial.begin(115200);  // start serial for output
+  Serial.println(i2c_init());
+  pinMode(22,INPUT_PULLUP);
+  pinMode(23,INPUT_PULLUP);
 
 
-  while (!Console) {    
-    ; // wait for Console port to connect.
-  }
+  //while (!Console) {    
+  //  ; // wait for Console port to connect.
+  //}
 
-  Console.println("Console Initialized");
+  Serial.println("Console Initialized");
   
   i2c_init();
-  Console.println("I2C Inialized");
+  Serial.println("I2C Inialized");
   scan();
 }
 
@@ -182,16 +182,16 @@ void scan()
   byte i = 0;
   for ( i= 0; i < 127; i++  )
   {
-    Console.print("Address: 0x");
-    Console.print(i,HEX);
+    Serial.print("Address: 0x");
+    Serial.print(i,HEX);
     bool ack = i2c_start(i<<1 | I2C_WRITE); 
     if ( ack ) {
-      Console.println(": OK");
-      Console.flush();
+      Serial.println(": OK");
+      Serial.flush();
     }
     else {
-      Console.println(": -");
-      Console.flush();      
+      Serial.println(": -");
+      Serial.flush();      
     }
     i2c_stop();
   }
@@ -201,31 +201,31 @@ void loop()
 {
   uint8_t length_read = 0;
 
-  Console.print("Manufacturer Name: ");
+  Serial.print("Manufacturer Name: ");
   length_read = i2c_smbus_read_block(MFG_NAME, i2cBuffer, bufferLen);
-  Console.write(i2cBuffer, length_read);
-  Console.println("");
+  Serial.write(i2cBuffer, length_read);
+  Serial.println("");
 
-  Console.print("Device Name: ");
+  Serial.print("Device Name: ");
   length_read = i2c_smbus_read_block(DEV_NAME, i2cBuffer, bufferLen);
-  Console.write(i2cBuffer, length_read);
-  Console.println("");
+  Serial.write(i2cBuffer, length_read);
+  Serial.println("");
 
-  Console.print("Chemistry ");
+  Serial.print("Chemistry ");
   length_read = i2c_smbus_read_block(CELL_CHEM, i2cBuffer, bufferLen);
-  Console.write(i2cBuffer, length_read);
-  Console.println("");
+  Serial.write(i2cBuffer, length_read);
+  Serial.println("");
 
-  // Console.print("Manufacturer Data ");
+  // Serial.print("Manufacturer Data ");
   // length_read = i2c_smbus_read_block(MFG_DATA, i2cBuffer, bufferLen);
-  // Console.write(i2cBuffer, length_read);
-  // Console.println("");
+  // Serial.write(i2cBuffer, length_read);
+  // Serial.println("");
   
-  Console.print("Design Capacity: " );
-  Console.println(fetchWord(DESIGN_CAPACITY));
+  Serial.print("Design Capacity: " );
+  Serial.println(fetchWord(DESIGN_CAPACITY));
   
-  Console.print("Design Voltage: " );
-  Console.println(fetchWord(DESIGN_VOLTAGE));
+  Serial.print("Design Voltage: " );
+  Serial.println(fetchWord(DESIGN_VOLTAGE));
   
   String formatted_date = "Manufacture Date (Y-M-D): ";
   int mdate = fetchWord(MFG_DATE);
@@ -237,68 +237,68 @@ void loop()
   formatted_date += mmonth;
   formatted_date += "-";
   formatted_date += mday;
-  Console.println(formatted_date);
+  Serial.println(formatted_date);
 
-  Console.print("Serial Number: ");
-  Console.println(fetchWord(SERIAL_NUM));
+  Serial.print("Serial Number: ");
+  Serial.println(fetchWord(SERIAL_NUM));
 
-  Console.print("Specification Info: ");
-  Console.println(fetchWord(SPEC_INFO));
+  Serial.print("Specification Info: ");
+  Serial.println(fetchWord(SPEC_INFO));
  
-  Console.print("Cycle Count: " );
-  Console.println(fetchWord(CYCLE_COUNT));
+  Serial.print("Cycle Count: " );
+  Serial.println(fetchWord(CYCLE_COUNT));
   
-  Console.print("Voltage: ");
-  Console.println((float)fetchWord(VOLTAGE)/1000);
+  Serial.print("Voltage: ");
+  Serial.println((float)fetchWord(VOLTAGE)/1000);
 
-  Console.print("Full Charge Capacity: " );
-  Console.println(fetchWord(FULL_CHARGE_CAPACITY));
+  Serial.print("Full Charge Capacity: " );
+  Serial.println(fetchWord(FULL_CHARGE_CAPACITY));
   
-  Console.print("Remaining Capacity: " );
-  Console.println(fetchWord(REMAINING_CAPACITY));
+  Serial.print("Remaining Capacity: " );
+  Serial.println(fetchWord(REMAINING_CAPACITY));
 
-  Console.print("Relative Charge(%): ");
-  Console.println(fetchWord(RELATIVE_SOC));
+  Serial.print("Relative Charge(%): ");
+  Serial.println(fetchWord(RELATIVE_SOC));
   
-  Console.print("Absolute Charge(%): ");
-  Console.println(fetchWord(ABSOLUTE_SOC));
+  Serial.print("Absolute Charge(%): ");
+  Serial.println(fetchWord(ABSOLUTE_SOC));
   
-  Console.print("Minutes remaining for full charge: ");
-  Console.println(fetchWord(TIME_TO_FULL));
+  Serial.print("Minutes remaining for full charge: ");
+  Serial.println(fetchWord(TIME_TO_FULL));
 
   // These aren't part of the standard, but work with some packs.
   // They don't work with the Lenovo and Dell packs we've tested
-  Console.print("Cell 1 Voltage: ");
-  Console.println(fetchWord(CELL1_VOLTAGE));
-  Console.print("Cell 2 Voltage: ");
-  Console.println(fetchWord(CELL2_VOLTAGE));
-  Console.print("Cell 3 Voltage: ");
-  Console.println(fetchWord(CELL3_VOLTAGE));
-  Console.print("Cell 4 Voltage: ");
-  Console.println(fetchWord(CELL4_VOLTAGE));
+  Serial.print("Cell 1 Voltage: ");
+  Serial.println(fetchWord(CELL1_VOLTAGE));
+  Serial.print("Cell 2 Voltage: ");
+  Serial.println(fetchWord(CELL2_VOLTAGE));
+  Serial.print("Cell 3 Voltage: ");
+  Serial.println(fetchWord(CELL3_VOLTAGE));
+  Serial.print("Cell 4 Voltage: ");
+  Serial.println(fetchWord(CELL4_VOLTAGE));
   
-  Console.print("State of Health: ");
-  Console.println(fetchWord(STATE_OF_HEALTH));
+  Serial.print("State of Health: ");
+  Serial.println(fetchWord(STATE_OF_HEALTH));
 
-  Console.print("Battery Mode (BIN): 0b");
-  Console.println(fetchWord(BATTERY_MODE),BIN);
+  Serial.print("Battery Mode (BIN): 0b");
+  Serial.println(fetchWord(BATTERY_MODE),BIN);
 
-  Console.print("Battery Status (BIN): 0b");
-  Console.println(fetchWord(BATTERY_STATUS),BIN);
+  Serial.print("Battery Status (BIN): 0b");
+  Serial.println(fetchWord(BATTERY_STATUS),BIN);
   
-  Console.print("Charging Current: ");
-  Console.println(fetchWord(CHARGING_CURRENT));
+  Serial.print("Charging Current: ");
+  Serial.println(fetchWord(CHARGING_CURRENT));
   
-  Console.print("Charging Voltage: ");
-  Console.println(fetchWord(CHARGING_VOLTAGE));
+  Serial.print("Charging Voltage: ");
+  Serial.println(fetchWord(CHARGING_VOLTAGE));
 
-  Console.print("Temp: ");
+  Serial.print("Temp: ");
   unsigned int tempk = fetchWord(TEMPERATURE);
-  Console.println((float)tempk/10.0-273.15);
+  Serial.println((float)tempk/10.0-273.15);
 
-  Console.print("Current (mA): " );
-  Console.println(fetchWord(CURRENT));
+  Serial.print("Current (mA): " );
+  Serial.println(fetchWord(CURRENT));
   
-  Console.println(".");
+  Serial.println(".");
   delay(1000);
 }
